a2d3a4530d64aee03b5b8b3ae526580fb2864891
booo <borgers@mi.fu-berlin.de>
2012-10-24 11:52:40 +0200

    Add skeleton for ex. 1

---
+++ ex01/README.md
@@ -0,0 +1,15 @@
+[Übungsblatt](http://page.mi.fu-berlin.de/panos/geom-comp/files/Ex1/exercises1.pdf)
+
+Ex. 1
+-----
+
+```
+aptitude install libcgal-dev
+```
+
+Ex. 2
+-----
+Done?
+
+Ex. 3
+-----
---
+++ ex01/src/exercise1.cpp
@@ -0,0 +1,28 @@
+
+#include <CGAL/Cartesian.h>
+
+// include other files here
+
+// Kernel type
+typedef CGAL::Cartesian<double> Kernel;
+
+// Point type
+typedef Kernel::Point_2 Point_2;
+
+// Segment type
+typedef Kernel::Segment_2 Segment_2;
+
+// Signature example of intersection1()
+bool intersection1(Segment_2&, Segment_2&){
+	// write your code here
+}
+
+// Put the other two functions here
+
+// Main program
+int main( int argc, char* argv[]) {
+
+	// Call your functions here and do some tests!
+
+	return 1;
+}
