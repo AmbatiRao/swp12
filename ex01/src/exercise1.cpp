b2c5f2966badfa32136a29059cbd28af56d5f9f1
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2012-10-26 19:33:06 +0200

    intersection of lines works in some cases

--- ex01/src/exercise1.cpp
+++ ex01/src/exercise1.cpp
@@ -1,6 +1,13 @@
-// vim: si:ts=4:sw=4:set expandtab
+// vim: si:ts=4:sw=4:expandtab
+
+/*
+ * TODO: implement own version of "oriented_side"
+ * TODO: implement own version of "has_on"
+ *
+ */
 
 #include <CGAL/Cartesian.h>
+#include <vector>
 
 /*
  * some convenience functions for printing
@@ -135,6 +142,78 @@ bool intersection1b(Segment_2 s1, Segment_2 s2){
     return side1 != side2 && side3 != side4;
 }
 
+CGAL::Object intersection2b(Segment_2 s1, Segment_2 s2){
+    // if there is no intersection, just return an empty object
+    if (!intersection1b(s1, s2)) {
+        return CGAL::Object();
+    }
+    // well, let's find out what kind of intersection this is
+
+    // get start end end points of segments
+    Point_2 p1 = s1.source();
+    Point_2 p2 = s1.target();
+    Point_2 p3 = s2.source();
+    Point_2 p4 = s2.target();
+    // create line objects from the points
+    Line_2 l1(p1, p2);
+    Line_2 l2(p3, p4);
+    // compute orientation of each point relative to the other line
+    Oriented_side side1 = l1.oriented_side(p3);
+    Oriented_side side2 = l1.oriented_side(p4);
+    Oriented_side side3 = l2.oriented_side(p1);
+    Oriented_side side4 = l2.oriented_side(p2);
+
+    // count the number of points that are on the respective other line
+    int onCount = 0;
+    onCount += side1 == CGAL::ON_ORIENTED_BOUNDARY;
+    onCount += side2 == CGAL::ON_ORIENTED_BOUNDARY;
+    onCount += side3 == CGAL::ON_ORIENTED_BOUNDARY;
+    onCount += side4 == CGAL::ON_ORIENTED_BOUNDARY;
+
+    if (onCount == 0) {
+        // simple, non-colinear intersection
+        // TODO: implement this
+        Point_2 p = Point_2(1,1);    
+        return make_object(p);
+    }
+
+    // collect points that are on the other segment
+    std::vector<Point_2> onPoints;
+    if (s1.has_on(p3)) {
+        onPoints.push_back(p3);
+    }
+    if (s1.has_on(p4)) {
+        onPoints.push_back(p4);
+    }
+    if (s2.has_on(p1)) {
+        onPoints.push_back(p1);
+    }
+    if (s2.has_on(p2)) {
+        onPoints.push_back(p2);
+    }
+    std::cout << "size: " << onPoints.size() << std::endl;
+
+    if (onPoints.size() == 1) {
+        // one point lies on the other line
+        Point_2 p = onPoints.at(0);
+        return make_object(p);
+    }
+
+    if (onPoints.size() >= 2) {
+        // lines are colinear.
+        // take the first two entries in the onPoints vector.
+        // this should be ok, even if there are more than 2 points in the
+        // vector because of the order in which we fill that vector.
+        Point_2 p = onPoints.at(0);
+        Point_2 q = onPoints.at(1);
+        Segment_2 s(p, q);
+        return make_object(s);
+    }
+
+    // in principal this is impossible
+    return CGAL::Object();
+}
+
 /*
  * helper functions that DO NOT use predicates or constructions
  */
@@ -145,7 +224,7 @@ void testForIntersectionAndPrint2(Segment_2 s1, Segment_2 s2){
 }
 
 void doIntersectionAndPrint2(Segment_2 s1, Segment_2 s2){
-    CGAL::Object result = intersection2a(s1, s2);
+    CGAL::Object result = intersection2b(s1, s2);
 
     if (const CGAL::Point_2<Kernel> *ipoint = CGAL::object_cast<CGAL::Point_2<Kernel> >(&result)) {
         // handle the point intersection case with *ipoint.
@@ -172,14 +251,17 @@ int main( int argc, char* argv[]) {
     Point_2 p3(2,1), p4(4,3);
     Point_2 p5(2,2), p6(6,2);
     Point_2 p7(1,4), p8(2,3);
+    Point_2 p9(3,2), p10(5,0);
 
-    // create two segments
+    // create some segments
     Segment_2 s1(p1, p2);
     Segment_2 s2(p3, p4);
     Segment_2 s3(p5, p6);
     Segment_2 s4(p7, p8);
+    Segment_2 s5(p9, p10);
 
     // test with predicate functions
+    echo("*****************************************************************");
     echo("Segment 1 and 2, they should intersect at point (3,2)");
     printSegments(s1, s2);
     testForIntersectionAndPrint(s1, s2);
@@ -198,23 +280,42 @@ int main( int argc, char* argv[]) {
     doIntersectionAndPrint(s1, s4);
     newline();
 
+    echo("Segment 2 and 5, they should intersect at point (3,2)");
+    printSegments(s2, s5);
+    testForIntersectionAndPrint(s2, s5);
+    doIntersectionAndPrint(s2, s5);
+    newline();
+
     // test with non-predicate functions
+    echo("*****************************************************************");
     echo("Segment 1 and 2, they should intersect at point (3,2)");
     printSegments(s1, s2);
     testForIntersectionAndPrint2(s1, s2);
-    //doIntersectionAndPrint2(s1, s2);
+    doIntersectionAndPrint2(s1, s2);
     newline();
 
     echo("Segment 1 and 3, they should intersect at segment (2,2), (4,2)");
     printSegments(s1, s3);
     testForIntersectionAndPrint2(s1, s3);
-    //doIntersectionAndPrint(s1, s3);
+    doIntersectionAndPrint2(s1, s3);
     newline();
 
     echo("Segment 1 and 4, they should not intersect");
     printSegments(s1, s4);
     testForIntersectionAndPrint2(s1, s4);
-    //doIntersectionAndPrint(s1, s4);
+    doIntersectionAndPrint2(s1, s4);
+    newline();
+
+    echo("Segment 2 and 5, they should intersect at point (3,2)");
+    printSegments(s2, s5);
+    testForIntersectionAndPrint2(s2, s5);
+    doIntersectionAndPrint2(s2, s5);
+    newline();
+
+    echo("Segment 2 and 2, they should intersect at segment (2,1), (4,3)");
+    printSegments(s2, s2);
+    testForIntersectionAndPrint2(s2, s2);
+    doIntersectionAndPrint2(s2, s2);
     newline();
 
     return 0;
