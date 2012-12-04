70c4beba60ce4cfedf63aff9e06fa54ea9ad06e1
booo <borgers@mi.fu-berlin.de>
2012-12-04 14:04:54 +0100

    add missing typedefs.h file

---
+++ triangulation/typedefs.h
@@ -0,0 +1,6 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Triangulation_3.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Triangulation_3<K> Triangulation;
+typedef Triangulation::Point Point;
