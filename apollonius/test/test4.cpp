5c1d5677abaf4aa98db6aea9aa1a45552c313593
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2012-12-07 20:14:15 +0100

    data changed, test4 showing the sites

--- apollonius/test/test4.cpp
+++ apollonius/test/test4.cpp
@@ -3,34 +3,22 @@
 #include <fstream>
 #include <cassert>
 
-// the number type
-#include <CGAL/MP_Float.h>
+//#include <map>
 
-typedef CGAL::MP_Float NT;
-
-// choose the kernel
-#include <CGAL/Simple_cartesian.h>
-
-typedef CGAL::Simple_cartesian<NT>  Kernel;
-
-// typedefs for the traits and the algorithm
-
-#include <CGAL/Apollonius_graph_2.h>
-#include <CGAL/Apollonius_graph_traits_2.h>
-
-typedef CGAL::Apollonius_graph_traits_2<Kernel>   Traits;
-//typedef CGAL::Apollonius_graph_2<Traits>          Apollonius_graph;
+// example that uses the filtered traits
 
 // choose the representation
 #include <CGAL/Simple_cartesian.h>
 
 typedef CGAL::Simple_cartesian<double> Rep;
 
+#include <CGAL/Apollonius_graph_2.h>
 #include <CGAL/Triangulation_data_structure_2.h>
 #include <CGAL/Apollonius_graph_vertex_base_2.h>
 #include <CGAL/Triangulation_face_base_2.h>
+#include <CGAL/Apollonius_graph_filtered_traits_2.h>
 
-// Qt stuff
+// Qt includes
 
 #include <CGAL/Qt/GraphicsViewNavigation.h>
 #include <QtGui/QApplication>
@@ -38,15 +26,36 @@ typedef CGAL::Simple_cartesian<double> Rep;
 #include <QtGui/QGraphicsView>
 #include <QLineF>
 
+// typedef for the traits; the filtered traits class is used
+typedef CGAL::Apollonius_graph_filtered_traits_2<Rep> Traits;
+
+// typedefs for the algorithm
+
+// With the second template argument in the vertex base class being
+// false, we indicate that there is no need to store the hidden sites.
+// One case where this is indeed not needed is when we only do
+// insertions, like in the main program below.
 typedef CGAL::Apollonius_graph_vertex_base_2<Traits,false>   Vb;
 typedef CGAL::Triangulation_face_base_2<Traits>              Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb>       Agds;
 typedef CGAL::Apollonius_graph_2<Traits,Agds>    Apollonius_graph;
 
-typedef Agds::Face_iterator All_faces_iterator;
-typedef Agds::Face Face;
-typedef Agds::Vertex_handle      Vertex_handle;
-typedef Agds::Vertex      Vertex;
+typedef Agds::Face 					Face;
+typedef Agds::Face_handle				Face_handle;
+typedef Agds::Vertex      				Vertex;
+typedef Agds::Vertex_handle     			Vertex_handle;
+
+typedef Agds::Face_iterator 				All_faces_iterator;
+typedef Agds::Edge_iterator 				All_edges_iterator;
+typedef Agds::Vertex_iterator 				All_vertices_iterator;
+typedef Apollonius_graph::Finite_faces_iterator		Finite_faces_iterator;
+typedef Apollonius_graph::Finite_edges_iterator		Finite_edges_iterator;
+typedef Apollonius_graph::Finite_vertices_iterator	Finite_vertices_iterator;
+
+typedef Traits::Object_2				Object_2;
+typedef Traits::Site_2					Site_2;
+typedef Traits::Point_2					Point_2;
+typedef Traits::Line_2					Line_2;
 
 int main(int argc , char* argv[])
 {
@@ -59,8 +68,8 @@ int main(int argc , char* argv[])
   int k = 0;
   // read the sites and insert them in the Apollonius graph
   while ( ifs >> site ) {
-    std::cout << "inserting: " << ++k << std::endl;
-    std::cout << site << std::endl;
+    std::cout << "inserting: " << ++k << ": " << site << std::endl;
+    std::cout << "position: " << site.point() << ", weight: " << site.weight() << std::endl;
     ag.insert(site);
     size_t nof = ag.number_of_faces ();
     std::cout << "number of faces: " << nof << std::endl;
@@ -73,15 +82,78 @@ int main(int argc , char* argv[])
   size_t nof = ag.number_of_faces ();
   std::cout << "number of faces: " << nof << std::endl;
 
-  for (All_faces_iterator fiter = ag.all_faces_begin (); fiter != ag.all_faces_end(); ++fiter) { 
-    Face face = *fiter;
-    Vertex_handle vertex = face.vertex(0);
-    Vertex v = *vertex;
-    //v.point();
-    //std::cout << "face: " << face << std::endl;
-    //std::cout << "vertex: " << vertex.point() << std::endl;
-    std::cout << "vertex: " << v << std::endl;
+  std::map<Vertex_handle,int> V;
+  Vertex_handle infinite_vertex;
+  int inum = 0;
+  V[infinite_vertex] = inum++;
+  for (Finite_vertices_iterator viter = ag.finite_vertices_begin();
+       viter != ag.finite_vertices_end(); ++viter) {
+       V[viter] = inum++;
+       std::cout << "vertex " << inum << ": " << viter->site() << std::endl;
+       // viter also has access to the hidden sites via hidden_sites_begin()...
   }
+
+  inum = 0;
+  for (All_faces_iterator fiter = ag.all_faces_begin (); fiter != ag.all_faces_end(); ++fiter) { 
+    std::cout << "face " << inum++ << ": ";
+    for (int j = 0; j < 3; j++) {
+      std::cout << V[ fiter->vertex(j) ] << " ";
+      // fiter also has acces to the facet-neighbors via neighbor()...
+    }
+    std::cout << std::endl;
+  }
+  
+  inum = 0;
+  for (All_faces_iterator fiter = ag.all_faces_begin (); fiter != ag.all_faces_end(); ++fiter) { 
+    std::cout << "face " << inum++ << ": ";
+
+    Object_2 o = ag.dual(fiter);
+    Site_2 site;
+    Line_2 line;
+    if (assign(site, o)) {
+      std::cout << "site";
+    } else if (assign(line, o)) {
+      std::cout << "line";
+    }
+    std::cout << std::endl;
+  }
+
+  /*
+   * for each site in the apo graph: get the surrounding faces. for each of
+   * those faces, get the dual, i.e. site/line. connecting the sites should
+   * form the cell boundary.
+   */
+
+  inum = 0;
+  for (All_edges_iterator eiter = ag.all_edges_begin (); eiter != ag.all_edges_end(); ++eiter) { 
+    //std::cout << "edge " << inum++ << ": ";
+    // ag.dual(*eiter); // Die ist leider private
+    //std::cout << std::endl;
+  }
+
+//  for (All_faces_iterator fiter = ag.all_faces_begin (); fiter != ag.all_faces_end(); ++fiter) { 
+//    Face face = *fiter;
+//    Vertex_handle vertex0 = face.vertex(0);
+//    Vertex_handle vertex1 = face.vertex(1);
+//    Vertex_handle vertex2 = face.vertex(2);
+//    Vertex v0 = *vertex0;
+//    Vertex v1 = *vertex1;
+//    Vertex v2 = *vertex2;
+//    std::cout << "vertices: " << v0 << v1 << v2 << std::endl;
+//    //std::cout << (fiter->neighbor(0)) << std::endl;
+//    //v.point();
+//    //std::cout << "face: " << face << std::endl;
+//    //std::cout << "vertex: " << vertex.point() << std::endl;
+//    //std::cout << "vertex: " << v << std::endl;
+//  }
+//
+//  for (Finite_faces_iterator fiter = ag.finite_faces_begin (); fiter != ag.finite_faces_end(); ++fiter) { 
+//    Face face = *fiter;
+//    std::cout << "face: " << face << std::endl;
+//  }
+
+  std::cout << "file_output:" << std::endl;
+  ag.file_output(std::cout);
 
   /*
    * visualize with Qt
@@ -96,6 +168,19 @@ int main(int argc , char* argv[])
     QLineF(100, 100, 200, 300),
     QPen(Qt::green, 3,  Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin)
   );
+
+  for (Finite_vertices_iterator viter = ag.finite_vertices_begin();
+       viter != ag.finite_vertices_end(); ++viter) {
+       Site_2 site = viter->site();
+       Point_2 p = site.point();
+       double weight = site.weight();
+       double size = weight * 2;
+
+       scene.addEllipse(
+         p.x() - weight, p.y() - weight, size, size,
+         QPen(Qt::red, 3,  Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin)
+       );
+  }
 
   // Prepare view, add scene, show
   QGraphicsView* view = new QGraphicsView(&scene);
