d481b3be030a0c18806cf8d39926a5635d5151fd
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-02-26 15:44:23 +0100

    add example for sql output

---
+++ doc/final-report/code/sql.c
@@ -0,0 +1,14 @@
+void writeSQL(Site_2 site, PointList polygon, char* outdir)
+{
+
+  std::cout << "INSERT INTO " << outdir << " (geom, id) VALUES (";
+  std::cout << "'POLYGON ((";
+  for (int i = 0; i < polygon.size(); i++) {
+    Point_2 p = polygon.at(i);
+    std::cout << p.x() << " " << p.y();
+    if (i < polygon.size() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << "))', " << site.id() << ");" << std::endl;
+}
